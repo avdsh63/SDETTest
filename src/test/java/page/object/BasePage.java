4dbf2cf00aee57f6fc86f62440b8b32670c65adb
avdsh63 <37176712+avdsh63@users.noreply.github.com>
2021-10-27 13:39:56 +0400

    second pull request

--- src/test/java/page/object/BasePage.java
+++ src/test/java/page/object/BasePage.java
@@ -8,6 +8,52 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
 
+//public class BasePage {
+//
+//    protected WebDriver driver;
+//
+//    public WebElement waitForElementPresentedByXpath(String xpath, String error_message) {
+//        WebDriverWait wait = new WebDriverWait(driver, 15);
+//        wait.withMessage(error_message);
+//        return wait.until(
+//                ExpectedConditions.presenceOfElementLocated(By.xpath(xpath)));
+//    }
+//
+//    public WebElement getElement(String xpath, String error_message) {
+//        WebElement element = waitForElementPresentedByXpath(xpath, error_message);
+//        return element;
+//    }
+//
+//    public WebElement waitForElementAndClickByXpath(String locator, String error_message) throws InterruptedException {
+//        WebElement element = waitForElementPresentedByXpath(locator, error_message);
+//        element.click();
+//        Thread.sleep(150);
+//        return element;
+//    }
+//
+//    public void sendKeysByXpath(String xpath, String text) {
+//        WebElement element = driver.findElement(By.xpath(xpath));
+//        element.sendKeys(text);
+//    }
+//
+//    public boolean elementTextEquals(String xpath, String equals) throws InterruptedException {
+//        Thread.sleep(1000);
+//        WebElement element = this.getElement(xpath, "can't find element");
+//        if (element.getText().equals(equals)) {
+//            return true;
+//        }
+//        else {
+//        return false;
+//        }
+//    }
+//
+//    public int messageCouter(String theme_text) throws InterruptedException {
+//        Thread.sleep(1000);
+//        List<WebElement> elements = driver.findElements(By.partialLinkText(theme_text));
+//        return elements.size();
+//    }
+//}
+
 public class BasePage {
 
     protected WebDriver driver;
@@ -43,7 +89,7 @@ public class BasePage {
             return true;
         }
         else {
-        return false;
+            return false;
         }
     }
 
@@ -53,3 +99,4 @@ public class BasePage {
         return elements.size();
     }
 }
+
